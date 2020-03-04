29b3bc07bd1301afcb8da8c06bf29f7b63835b58
Mustapha Mastour <mustaphamastour@MacBook-Pro-de-Mustapha.local>
2020-03-04 19:34:13 +0100

    add the parser

---
+++ parser/README.md
@@ -0,0 +1,14 @@
+*Comment lancer le Parser de Certificat x509*
+------
+
+
+##Pour installer le script (root du dossier):
+
+> npm install
+
+Pour le lancer il vous faut un dossier contenant des certificats pem pour pouvoir le parser et récuperer les données qui nous intéresse
+
+##Lancer la commande suivante 
+
+> node index.js dossier_de_certificats/
+
---
+++ parser/worker.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+const { Certificate } = require('@fidm/x509');
+const { Executors } = require('node-threadpool');
+const perf = require('execution-time')();
+
+perf.start();
+
+const pemDirectory = cleanDirname(process.argv[2]);
+
+const json = {
+    rsa: [],
+    ecc: []
+}
+
+fs.readdir(pemDirectory, (err, files) => {
+
+    files.forEach(file => {
+
+        var pathFile = pemDirectory + "/" + file
+
+        const issuer = Certificate.fromPEM(fs.readFileSync(pathFile));
+        const parsedCert = {
+            keyType: issuer.publicKey.algo,
+            dnsNames: issuer.dnsNames.toString(),
+            ipAdressess: issuer.ipAddresses.toString(),
+            publicKeyRaw: issuer.publicKeyRaw,
+            commonName: issuer.issuer.commonName,
+            countryName: issuer.issuer.countryName
+        }
+
+        if (parsedCert.keyType == "rsaEncryption") {
+            json.rsa.push(parsedCert)
+        }
+        else {
+            json.ecc.push(parsedCert)
+        }
+
+    });
+
+    console.log(json);
+
+    const result_perf = perf.stop();
+
+    console.log("\n-------------------------------------\n")
+    console.log("Execution time: " + result_perf.words);
+
+    return json;
+
+});
+
+
+function cleanDirname(dir) {
+    if(dir[dir.length - 1] === '/') {
+        return dir.slice(0, -1)
+    }
+    else {
+        return dir;
+    }
+}
+
+
+
+// const fs = require('fs');
+// const { Certificate } = require('@fidm/x509');
+// const perf = require('execution-time')();
+
+// perf.start();
+
+// const pemDirectory = cleanDirname(process.argv[2]);
+
+// const json = {
+//     rsa: [],
+//     ecc: []
+// }
+
+// fs.readdir(pemDirectory, (err, files) => {
+
+//     files.forEach(file => {
+
+//         var pathFile = pemDirectory + "/" + file
+
+//         const issuer = Certificate.fromPEM(fs.readFileSync(pathFile));
+//         const parsedCert = {
+//             keyType: issuer.publicKey.algo,
+//             dnsNames: issuer.dnsNames.toString(),
+//             ipAdressess: issuer.ipAddresses.toString(),
+//             publicKeyRaw: issuer.publicKeyRaw,
+//             commonName: issuer.issuer.commonName,
+//             countryName: issuer.issuer.countryName
+//         }
+
+//         if (parsedCert.keyType == "rsaEncryption") {
+//             json.rsa.push(parsedCert)
+//         }
+//         else {
+//             json.ecc.push(parsedCert)
+//         }
+
+//     });
+
+//     console.log(json);
+
+//     const result_perf = perf.stop();
+
+//     console.log("\n-------------------------------------\n")
+//     console.log("Execution time: " + result_perf.words);
+
+//     return json;
+
+// });
+
+
+// function cleanDirname(dir) {
+//     if(dir[dir.length - 1] === '/') {
+//         return dir.slice(0, -1)
+//     }
+//     else {
+//         return dir;
+//     }
+// }
