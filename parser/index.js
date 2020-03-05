3201211d2f8f1956e857a8cc2f588d33a0a92520
Mustapha Mastour <mustaphamastour@MacBook-Pro-de-Mustapha.local>
2020-03-05 18:27:16 +0100

    update sample.json

--- parser/index.js
+++ parser/index.js
@@ -1,46 +1,56 @@
-'use strict'
 
-const { Worker } = require('worker_threads')
+import { Executors } from "node-threadpool";
+const {Executors } = require("node-threadpool")
+ 
+const pool = Executors.newFixedThreadPool(1);
+const result = pool.submit(async () => "hello world");
+ 
+console.log(await result);
 
-/**
-* Use a worker via Worker Threads module to make intensive CPU task
-* @param filepath string relative path to the file containing intensive CPU task code
-* @return {Promise(mixed)} a promise that contains result from intensive CPU task
-*/
 
-function _useWorker (filepath, argument) {
+// 'use strict'
 
-  return new Promise((resolve, reject) => {
+// const { Worker } = require('worker_threads')
 
-    const worker = new Worker(filepath, {argv: [argument]})
-    worker.on('online', () => { console.log('Launching intensive CPU task') })
+// /**
+// * Use a worker via Worker Threads module to make intensive CPU task
+// * @param filepath string relative path to the file containing intensive CPU task code
+// * @return {Promise(mixed)} a promise that contains result from intensive CPU task
+// */
 
-    worker.on('message', messageFromWorker => {
-      console.log(messageFromWorker)
-      return resolve
-    })
+// function _useWorker (filepath, argument) {
 
-    worker.on('error', reject)
-    worker.on('exit', code => {
+//   return new Promise((resolve, reject) => {
 
-      if (code !== 0) {
-        reject(new Error(`Worker stopped with exit code ${code}`))
-      }
+//     const worker = new Worker(filepath, {argv: [argument]})
+//     worker.on('online', () => { console.log('Launching intensive CPU task') })
 
-    })
-  })
-}
+//     worker.on('message', messageFromWorker => {
+//       console.log(messageFromWorker)
+//       return resolve
+//     })
 
-/**
-* Use main thread while making intensive CPU task on worker
-*/
+//     worker.on('error', reject)
+//     worker.on('exit', code => {
 
-async function main () {
+//       if (code !== 0) {
+//         reject(new Error(`Worker stopped with exit code ${code}`))
+//       }
 
-  // this log will happen every second during and after the intensive task, main thread is never blocked
+//     })
+//   })
+// }
 
-  setInterval(() => { console.log('Event loop on main thread is not blocked right now') }, 1000)
-  await _useWorker('./worker.js', process.argv[3])
-}
+// /**
+// * Use main thread while making intensive CPU task on worker
+// */
 
-main()
+// async function main () {
+
+//   // this log will happen every second during and after the intensive task, main thread is never blocked
+
+//   setInterval(() => { console.log('Event loop on main thread is not blocked right now') }, 1000)
+//   await _useWorker('./worker.js', process.argv[3])
+// }
+
+// main()
--- parser/worker.js
+++ parser/worker.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const { Certificate } = require('@fidm/x509');
-const { Executors } = require('node-threadpool');
 const perf = require('execution-time')();
 
 perf.start();
@@ -12,12 +11,13 @@ const json = {
     ecc: []
 }
 
+
+process.env.UV_
 fs.readdir(pemDirectory, (err, files) => {
 
     files.forEach(file => {
 
         var pathFile = pemDirectory + "/" + file
-
         const issuer = Certificate.fromPEM(fs.readFileSync(pathFile));
         const parsedCert = {
             keyType: issuer.publicKey.algo,
@@ -39,6 +39,9 @@ fs.readdir(pemDirectory, (err, files) => {
 
     console.log(json);
 
+    let data = JSON.stringify(json);
+    fs.writeFileSync('certificat.json', data);
+
     const result_perf = perf.stop();
 
     console.log("\n-------------------------------------\n")
@@ -57,64 +60,3 @@ function cleanDirname(dir) {
         return dir;
     }
 }
-
-
-
-// const fs = require('fs');
-// const { Certificate } = require('@fidm/x509');
-// const perf = require('execution-time')();
-
-// perf.start();
-
-// const pemDirectory = cleanDirname(process.argv[2]);
-
-// const json = {
-//     rsa: [],
-//     ecc: []
-// }
-
-// fs.readdir(pemDirectory, (err, files) => {
-
-//     files.forEach(file => {
-
-//         var pathFile = pemDirectory + "/" + file
-
-//         const issuer = Certificate.fromPEM(fs.readFileSync(pathFile));
-//         const parsedCert = {
-//             keyType: issuer.publicKey.algo,
-//             dnsNames: issuer.dnsNames.toString(),
-//             ipAdressess: issuer.ipAddresses.toString(),
-//             publicKeyRaw: issuer.publicKeyRaw,
-//             commonName: issuer.issuer.commonName,
-//             countryName: issuer.issuer.countryName
-//         }
-
-//         if (parsedCert.keyType == "rsaEncryption") {
-//             json.rsa.push(parsedCert)
-//         }
-//         else {
-//             json.ecc.push(parsedCert)
-//         }
-
-//     });
-
-//     console.log(json);
-
-//     const result_perf = perf.stop();
-
-//     console.log("\n-------------------------------------\n")
-//     console.log("Execution time: " + result_perf.words);
-
-//     return json;
-
-// });
-
-
-// function cleanDirname(dir) {
-//     if(dir[dir.length - 1] === '/') {
-//         return dir.slice(0, -1)
-//     }
-//     else {
-//         return dir;
-//     }
-// }
