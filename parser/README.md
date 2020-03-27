09d7f8ac9bc6325717377e1dca1f0eea918d18fc
Mustapha Mastour <mustaphamastour@MacBook-Pro-de-Mustapha.local>
2020-03-27 14:41:59 +0100

    remove double checker

--- parser/README.md
+++ parser/README.md
@@ -4,11 +4,10 @@
 
 ##Pour installer le script (root du dossier):
 
+> cd parser 
 > npm install
 
 Pour le lancer il vous faut un dossier contenant des certificats pem pour pouvoir le parser et récuperer les données qui nous intéresse
-
-##Lancer la commande suivante 
 
 > node index.js dossier_de_certificats/
 
--- parser/index.js
+++ parser/index.js
@@ -1,65 +1,55 @@
-const fs = require('fs');
-const { Certificate } = require('@fidm/x509');
-const perf = require('execution-time')();
-var forge = require('node-forge');
+const fs = require("fs");
+const { Certificate } = require("@fidm/x509");
+const perf = require("execution-time")();
+var forge = require("node-forge");
 
 perf.start();
 
 const pemDirectory = cleanDirname(process.argv[2]);
 
-const json = {}
-const doubleChecker = []
+const json = {};
 
 fs.readdir(pemDirectory, (err, files) => {
-    var idCert = 1;
-    files.forEach(file => {
+  var idCert = 1;
+  files.forEach(file => {
+    var pathFile = pemDirectory + "/" + file;
+    try {
+      const issuer = Certificate.fromPEM(fs.readFileSync(pathFile));
 
-        var pathFile = pemDirectory + "/" + file
-        try {
-            const issuer = Certificate.fromPEM(fs.readFileSync(pathFile));
+      const parsedCert = {
+        keyType: issuer.publicKey.algo,
+        dnsNames: issuer.dnsNames.toString().split(","),
+        ipAdressess: issuer.ipAddresses.toString().split(","),
+        publicKeyRaw: Buffer.from(
+          issuer.publicKey.toJSON().publicKey.toJSON().data
+        ).toString("hex"),
+        commonName: issuer.issuer.commonName,
+        countryName: issuer.issuer.countryName
+      };
 
-            const parsedCert = {
-                keyType: issuer.publicKey.algo,
-                dnsNames: issuer.dnsNames.toString().split(','),
-                ipAdressess: issuer.ipAddresses.toString().split(','),
-                publicKeyRaw: Buffer.from(issuer.publicKey.toJSON().publicKey.toJSON().data).toString('hex'),
-                commonName: issuer.issuer.commonName,
-                countryName: issuer.issuer.countryName
-            }
+      json[idCert] = parsedCert;
+      idCert++;
+      console.log(idCert);
+      
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-            if (!doubleChecker.includes(JSON.stringify(parsedCert))) {
-                json[idCert] = parsedCert
-                idCert++;
-                doubleChecker.push(JSON.stringify(parsedCert))
-                console.log(idCert)
-            }
-            
-        }
-        catch (err) {
-            console.log(err)
-        }
+  let data = JSON.stringify(json);
+  fs.writeFileSync("certificat.json", data);
 
-    });
+  const result_perf = perf.stop();
 
-    
-    let data = JSON.stringify(json);
-    console.log(doubleChecker.length)
-    fs.writeFileSync('certificat.json', data);
+  console.log("Execution time: " + result_perf.words);
 
-    const result_perf = perf.stop();
-
-    console.log("Execution time: " + result_perf.words);
-
-    return json;
-
+  return json;
 });
 
-
 function cleanDirname(dir) {
-    if(dir[dir.length - 1] === '/') {
-        return dir.slice(0, -1)
-    }
-    else {
-        return dir;
-    }
+  if (dir[dir.length - 1] === "/") {
+    return dir.slice(0, -1);
+  } else {
+    return dir;
+  }
 }
